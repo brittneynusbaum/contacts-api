611df5aa4811751a137d916c4625bc7cc090ffde
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-12 21:52:02 -0800

    adds init and contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+contact4 = Contact.new(
+  first_name: "Frodo",
+  last_name: "Baggins",
+  email: "frodobaggins@gmail.com",
+  phone_number: "555-111-0121"
+)
+
+contact5 = Contact.new(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name.
+  email: Faker::Internet.email,
+  phone_number: Faker::PhoneNumber.phone_number
+)
+p contact5
