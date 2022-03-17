350d874a7ed0afcb375f071b3c633a93c8c6a7e4
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 00:22:13 -0700

    adds address as geocoder param, converts to latitude/longitude

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,17 @@
 class ContactsController < ApplicationController
 
   def create
-    results = Geocoder.search(address: params[:address])
-    render json: results.first.coordinates
+    results = Geocoder.search(params[:address])
     contact = Contact.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       phone_number: params[:phone_number],
-      latitude: params[:latitude],
-      longitude: params[:longitude],
+      latitude: results.first.coordinates[0],
+      longitude: results.first.coordinates[1],
       address: params[:address]      
     )
-
-    # render json: contact.as_json
+    render json: contact.as_json
   end 
 
 end
