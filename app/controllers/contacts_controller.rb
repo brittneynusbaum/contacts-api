3f180c23bbaaa5288804ac5ee67778afc179b072
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 00:11:27 -0700

    adds geocoder

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,19 @@
 class ContactsController < ApplicationController
 
   def create
-    # results = Geocoder.search("Paris")
-    # render json: results.first.coordinates
+    results = Geocoder.search(address: params[:address])
+    render json: results.first.coordinates
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      latitude: params[:latitude],
+      longitude: params[:longitude],
+      address: params[:address]      
+    )
 
-    render json: {message: "testing create"}
+    # render json: contact.as_json
   end 
 
 end
